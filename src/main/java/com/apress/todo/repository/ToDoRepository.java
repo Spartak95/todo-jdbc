eb44f6bad3b8c91439ed5a0a424f90ec3543e415
spartak.gareginyan <gareginyans@mail.ru>
2023-11-08 09:56:32 +0400

    I have finalized the todo jdbc project.

--- src/main/java/com/apress/todo/repository/ToDoRepository.java
+++ src/main/java/com/apress/todo/repository/ToDoRepository.java
@@ -1,9 +1,14 @@
 package com.apress.todo.repository;
 
 import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.apress.todo.domain.ToDo;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -16,6 +21,7 @@ public class ToDoRepository implements CommonRepository<ToDo> {
     private static final String SQL_QUERY_FIND_BY_ID = SQL_QUERY_FIND_ALL + " where id = :id";
     private static final String SQL_UPDATE =
         "update todo set description = :description, modified = :modified, completed = :completed where id = :id";
+    private static final String SQL_DELETE = "delete from todo where id = :id";
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public ToDoRepository(NamedParameterJdbcTemplate jdbcTemplate) {
@@ -25,32 +31,66 @@ public class ToDoRepository implements CommonRepository<ToDo> {
     private RowMapper<ToDo> toDoRowMapper = (ResultSet rs, int rowNum) -> {
         ToDo toDo = new ToDo();
         toDo.setId(rs.getString("id"));
+        toDo.setDescription(rs.getString("description"));
+        toDo.setCreated(rs.getTimestamp("created").toLocalDateTime());
+        toDo.setCompleted(rs.getBoolean("completed"));
 
         return toDo;
     };
 
     @Override
     public ToDo save(ToDo domain) {
-        return null;
+        ToDo result = findById(domain.getId());
+
+        if (result != null) {
+            result.setDescription(domain.getDescription());
+            result.setCompleted(domain.isCompleted());
+            result.setModified(LocalDateTime.now());
+
+            return upsert(domain, SQL_UPDATE);
+        }
+
+        return upsert(domain, SQL_INSERT);
+    }
+
+    private ToDo upsert(final ToDo toDo, String sql) {
+        Map<String, Object> namedParameters = new HashMap<>();
+        namedParameters.put("id", toDo.getId());
+        namedParameters.put("description", toDo.getDescription());
+        namedParameters.put("created", Timestamp.valueOf(toDo.getCreated()));
+        namedParameters.put("modified", Timestamp.valueOf(toDo.getModified()));
+        namedParameters.put("completed", toDo.isCompleted());
+
+        this.jdbcTemplate.update(sql, namedParameters);
+
+        return findById(toDo.getId());
     }
 
     @Override
     public Iterable<ToDo> save(Collection<ToDo> domains) {
-        return null;
+        domains.forEach(this::save);
+        return findAll();
     }
 
     @Override
     public void delete(ToDo domain) {
-
+        Map<String, String> namedParameters = Map.of("id", domain.getId());
+        this.jdbcTemplate.update(SQL_DELETE, namedParameters);
     }
 
     @Override
     public ToDo findById(String id) {
-        return null;
+        Map<String, String> namedParameters = Map.of("id", id);
+
+        try {
+            return this.jdbcTemplate.queryForObject(SQL_QUERY_FIND_BY_ID, namedParameters, toDoRowMapper);
+        } catch (EmptyResultDataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
     public Iterable<ToDo> findAll() {
-        return null;
+        return this.jdbcTemplate.query(SQL_QUERY_FIND_ALL, toDoRowMapper);
     }
 }
